2b51c1533a609dc95b3bcba6487b02a532c311c5
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 16:30:18 +0100

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,44 @@ import ExpenseElement from "./components/ExpenseElement";
 import TestComp from "./components/TestComp";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: "New TV",
+      price: 200,
+      date: new Date("2025-02-12"),
+    },
+    {
+      id: 2,
+      title: "Nike Air Force",
+      price: 300,
+      date: new Date("2023-10-22"),
+    },
+    {
+      id: 3,
+      title: "Phone",
+      price: 500,
+      date: new Date("2025-05-12"),
+    },
+    {
+      id: 4,
+      title: "Education",
+      price: 150,
+      date: new Date("2024-02-25"),
+    },
+  ];
   return (
     <>
-      <ExpenseElement />
+      {data.map((item) => {
+        return (
+          <ExpenseElement
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            date={item.date}
+          />
+        );
+      })}
     </>
   );
 }
--- src/components/ExpenseElement.jsx
+++ src/components/ExpenseElement.jsx
@@ -1,11 +1,11 @@
 import "./ExpenseElement.css";
-const ExpenseElement = () => {
+const ExpenseElement = (props) => {
   return (
     <div className="expense-item">
-      <div className="expense-date">date</div>
+      <div className="expense-date">{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>title</h2>
-        <div className="expense-item__price">price</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.price}</div>
       </div>
     </div>
   );
