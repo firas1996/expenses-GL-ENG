1c7ae860c18865898e1884091597c861169e4a3b
firas hamila <firas.hm.1996@gmail.com>
2025-02-12 16:50:04 +0100

    a

--- src/components/ExpenseElement.jsx
+++ src/components/ExpenseElement.jsx
@@ -1,11 +1,19 @@
 import "./ExpenseElement.css";
-const ExpenseElement = (props) => {
+const ExpenseElement = ({ title, price, date }) => {
+  //   const { title, price, date } = props;
+  const year = date.getFullYear();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div className="expense-date">{props.date.toISOString()}</div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">$ {props.price}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
   );
