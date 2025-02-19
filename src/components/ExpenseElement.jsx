8196629b3f28898a992ca4d87b233282d4bfbce2
firas hamila <firas.hm.1996@gmail.com>
2025-02-19 14:08:46 +0100

    a

--- src/components/ExpenseElement.jsx
+++ src/components/ExpenseElement.jsx
@@ -4,6 +4,7 @@ const ExpenseElement = ({ title, price, date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
+  console.log("object");
   return (
     <div className="expense-item">
       <div className="expense-date">
